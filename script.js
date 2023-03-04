23c542f0c0d29b6c15245749a65b032945eed5b2
Manoel Matias <manoel13matias@gmail.com>
2023-03-04 03:15:09 -0300

    Criei os arquivos.

---
+++ script.js
@@ -0,0 +1,4 @@
+const body = document.querySelector("body"), ponteiroHora = document.querySelector(".hora"),
+ponteiroMinuto = document.querySelector(".minuto"),
+ponteirosegundo = document.querySelector(".segundo"),
+botao = document.querySelector(".botao");
