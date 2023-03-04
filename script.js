9b4e02d3fac7888e51a3c5383861e10398f74787
Manoel Matias <manoel13matias@gmail.com>
2023-03-04 17:02:11 -0300

    Alterei o arquivo JS

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-const body = document.querySelector("body"), ponteiroHora = document.querySelector(".hora"),
-ponteiroMinuto = document.querySelector(".minuto"),
-ponteirosegundo = document.querySelector(".segundo"),
-botao = document.querySelector(".botao");
+const BODY = document.querySelector("body");
+const PONTEIROHORA = document.querySelector(".hora");
+const PONTEIROMINUTO = document.querySelector(".minuto");
+const PONTEIROSEGUNDO = document.querySelector(".segundo");
+const BOTAO = document.querySelector(".botao");
+
+if(localStorage.getItem("modo") === "Modo Escuro")
+{
+    BODY.classList.add("escuro");
+    BOTAO.textContent = "Modo Claro";
+}
+else
+{
+    BOTAO.textContent = "Modo Escuro";
+}
+
+BOTAO.addEventListener("click", () =>
+{
+    BODY.classList.toggle("escuro");
+    const EMODOESCURO = BODY.classList.contains("escuro");
+    BOTAO.textContent = EMODOESCURO ? "Modo Claro" : "Modo Escuro";
+    localStorage.setItem("modo", EMODOESCURO ? "Modo Escuro" : "Modo Claro");
+});
+
+const atualizarHora = () =>
+{
+    let data = new Date(),
+    segundoParaGrau = (data.getSeconds() / 60) * 360;
+    minutoParaGrau = (data.getMinutes() / 60) * 360;
+    horaParaGrau = (data.getHours() / 12) * 360;
+    PONTEIROSEGUNDO.style.transform = `rotate(${segundoParaGrau}deg)`;
+    PONTEIROMINUTO.style.transform = `rotate(${minutoParaGrau}deg)`;
+    PONTEIROHORA.style.transform = `rotate(${horaParaGrau}deg)`;
+};
+
+setInterval(atualizarHora, 1000);
+atualizarHora();
